8c41289c42b2281f08a1d7b2cd0411ce8e473275
Ganiev <ganievhushbaht@yandex.ru>
2023-04-06 02:41:13 +0300

    feat: add header

---
+++ src/components/vHeader.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="header__wrapper">
+        <div class="header__left-block">
+          <img
+            src="@/assets/images/icons/logo.svg"
+            alt="logo"
+            class="header__logo"
+          />
+          <ul class="header__list">
+            <a href="#" class="header__link"
+              ><li class="header__item">Продукты</li></a
+            >
+            <a href="#" class="header__link"
+              ><li class="header__item">Цвета</li></a
+            >
+            <a href="#" class="header__link"
+              ><li class="header__item">Вдохновение</li></a
+            >
+            <a href="#" class="header__link"
+              ><li class="header__item">Советы</li></a
+            >
+            <a href="#" class="header__link"
+              ><li class="header__item">Найти магазин</li></a
+            >
+          </ul>
+        </div>
+        <div class="header__right-block">
+          <div class="header__phone">
+            <p class="header__tel">+7 (495) 221-77-69</p>
+            <button class="header__del-tel">Заказать звонок</button>
+          </div>
+          <div class="header__navigation">
+            <button class="header__btn">
+              <img
+                src="@/assets/images/icons/search.svg"
+                alt="search"
+                class="header__search"
+              />
+            </button>
+            <button class="header__btn">
+              <img
+                src="@/assets/images/icons/profile.svg"
+                alt="search"
+                class="header__search"
+              />
+            </button>
+            <button class="header__btn">
+              <img
+                src="@/assets/images/icons/heart.svg"
+                alt="search"
+                class="header__search"
+              />
+            </button>
+            <button class="header__btn">
+              <img
+                src="@/assets/images/icons/shoper.svg"
+                alt="search"
+                class="header__search"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.container {
+  max-width: 1700px;
+  margin: 0 auto;
+}
+.header {
+  padding: 45px 65px 45px 65px;
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+	&__left-block{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 900px;
+	}
+  &__list {
+    display: flex;
+  }
+  &__item {
+    margin-right: 25px;
+  }
+	&__link{
+		font-weight: 400;
+		font-size: 14px;
+		line-height: 100%;
+		letter-spacing: 0.06em;
+		text-transform: uppercase;
+		color: #1F2020;
+	}
+	&__right-block{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 400px;
+	}
+	&__tel{
+		font-weight: 500;
+		font-size: 16px;
+		line-height: 100%;
+		letter-spacing: -0.02em;
+		color: #1F2020;
+	}
+	&__navigation{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 150px;
+	}
+	&__del-tel{
+		background: none;
+		font-weight: 400;
+		font-size: 14px;
+		line-height: 100%;
+		color: #1F2020;
+		opacity: 0.3;
+	}
+  &__btn {
+    background: none;
+  }
+}
+
+.header__list:last-child {
+  margin-right: 0;
+}
+</style>
