0433abb08fc54befe6c8e48cf12be6d5a8221d71
Ganiev <ganievhushbaht@yandex.ru>
2023-04-07 21:59:28 +0300

    feat: added slider navigation

--- src/components/vSlider.vue
+++ src/components/vSlider.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="promo">
     <div class="container">
+      <div class="promo__pagination-text">
+        <button class="promo__btn">ГЛАВНАЯ</button>
+        <img
+          src="@/assets/images/icons/ellipse.svg"
+          alt="ellip"
+          class="promo__ellipse"
+        />
+        <button class="promo__btn">ПРОДУКТЫ</button>
+        <img
+          src="@/assets/images/icons/ellipse.svg"
+          alt="ellip"
+          class="promo__ellipse"
+        />
+        <button class="promo__btn">КРАСКИ</button>
+      </div>
       <div class="swiper">
-        <div class="promo__pagination-text">
-          <button class="promo__btn">ГЛАВНАЯ</button>
-          <button class="promo__btn">ПРОДУКТЫ</button>
-          <button class="promo__btn">КРАСКИ</button>
-        </div>
         <swiper
           :modules="modules"
           :slides-per-view="1"
-          :pagination="{ clickable: true }"
           :scrollbar="{ draggable: true }"
+          :navigation="{
+            nextEl: '.nextArrow',
+            prevEl: '.prevArrow',
+          }"
           @swiper="onSwiper"
           @slideChange="onSlideChange"
           :loop="true"
         >
+				<div class="nav-indicator nextArrow">
+            <img src="@/assets/images/icons/arrow.svg" alt="arrow" />
+				</div>
           <swiper-slide v-for="items in arrSlider" :key="items.id">
             <div class="promo__box">
-							<img :src="require(`@/assets/images/${items.image}`)" alt="">
+              <img
+                :src="require(`@/assets/images/${items.image}`)"
+                class="promo__img"
+                alt="image"
+              />
               <div class="promo__text-in-img">
-                <h1 class="promo__title">{{items.title}}</h1>
-                <h3 class="promo__subtitle">{{items.subtitle}}</h3>
+                <h1 class="promo__title">{{ items.title }}</h1>
+                <h3 class="promo__subtitle">{{ items.subtitle }}</h3>
               </div>
             </div>
           </swiper-slide>
@@ -31,6 +51,7 @@
   </div>
 </template>
 <script>
+import { Navigation, Pagination, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/scss";
 import "swiper/scss/navigation";
@@ -47,14 +68,14 @@ export default {
         {
           id: Date.now(),
           title: "Краски",
-					image: 'mainSlider.png',
+          image: "mainSlider.png",
           subtitle:
             "Идеально подходят для стен и других поверхностей. Найди свой идеальный цвет!",
         },
         {
           id: Date.now(),
           title: "Краски",
-					image: 'mainSlider.png',
+          image: "mainSlider.png",
           subtitle:
             "Идеально подходят для стен и других поверхностей. Найди свой идеальный цвет!",
         },
@@ -62,55 +83,70 @@ export default {
           id: Date.now(),
           image: "mainSlider.png",
           title: "Краски",
-					image: 'mainSlider.png',
+          image: "mainSlider.png",
           subtitle:
             "Идеально подходят для стен и других поверхностей. Найди свой идеальный цвет!",
         },
         {
           id: Date.now(),
           title: "Краски",
-					image: 'mainSlider.png',
+          image: "mainSlider.png",
           subtitle:
             "Идеально подходят для стен и других поверхностей. Найди свой идеальный цвет!",
         },
       ],
     };
   },
+  setup() {
+    return {
+      modules: [Navigation, Pagination, Autoplay],
+    };
+  },
 };
 </script>
 <style lang="scss" scoped>
-.container{
+.container {
   width: 100%;
   margin: 0 auto;
 }
 .promo {
-	width: 100%;
-	&__pagination-text{
-		position: absolute;
-		z-index: 10;
-		top: 10%;
-		left: 10%;
-	}
-	&__text-in-img{
-		position: absolute;
-		top: 40%;
-		left: 40%;
-	}
-	&__title{
-		font-weight: 400;
-		font-size: 72px;
-		line-height: 88%;
-		letter-spacing: -0.02em;
-		color: #FFFFFF;
-	}
-	&__subtitle{
-		font-weight: 500;
-		font-size: 16px;
-		line-height: 130%;
-		text-align: center;
-		letter-spacing: 0.02em;
-		color: #FFFFFF;
-	}
+	position: relative;
+	&__pagination-text {
+    width: 190px;
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    z-index: 10;
+    top: 32px;
+    left: 64px;
+  }
+  &__text-in-img {
+    position: absolute;
+    top: 35%;
+    left: 40%;
+  }
+  &__img {
+    min-width: 100%;
+    max-height: 560px;
+  }
+  &__title {
+    font-weight: 400;
+    font-size: 72px;
+    text-align: center;
+    line-height: 88%;
+    letter-spacing: -0.02em;
+    color: #ffffff;
+  }
+  &__subtitle {
+    font-weight: 500;
+    max-width: 472px;
+    font-size: 16px;
+    line-height: 130%;
+    text-align: center;
+    letter-spacing: 0.02em;
+    color: #ffffff;
+    margin-top: 25px;
+  }
   &__btn {
     background: none;
     font-weight: 400;
@@ -118,16 +154,23 @@ export default {
     line-height: 100%;
     letter-spacing: 0.06em;
     text-transform: uppercase;
-    color: #ce0303;
+    color: #ffffff;
     opacity: 0.3;
   }
 }
-.swiper{
-	position: relative;
+.nextArrow {
+  position: absolute;
+  cursor: pointer;
+  z-index: 10;
+  top: 355px;
+  right: 15%;
 }
-
-img{
-	min-width: 100%;
-	max-height: 560px;
+@media (max-width: 1500px) {
+  .promo {
+    &__text-in-img {
+    	top: 35%;
+    	left: 35%;
+    }
+  }
 }
 </style>
