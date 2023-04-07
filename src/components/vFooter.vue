84bc3a1f3b665a6cbc4bbd5f3045d59c0c9243b5
Ganiev <ganievhushbaht@yandex.ru>
2023-04-07 22:09:35 +0300

    feat: added footer

---
+++ src/components/vFooter.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="footer"></div>
+</template>
+<script>
+export default {
+	name: 'vFooter'
+}
+</script>
+<style lang="scss" scoped>
+.footer{
+	background: #000000;
+	height: 300px;
+	width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="mainView">
-    <vHeader msg="Welcome to Your Vue.js App"/>
-		<vSlider></vSlider>
+		<div class="wrapper">
+			<div>
+    		<vHeader/>
+				<vSlider/>
+			</div>
+			<vFooter/>
+		</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import vHeader from '@/components/vHeader'
-import vSlider from '@/components/vSlider';
+import vHeader from '@/components/vHeader.vue'
+import vSlider from '@/components/vSlider.vue'
+import vFooter from '@/components/vFooter.vue';
 
 export default {
   name: 'HomeView',
   components: {
     vHeader,
-		vSlider
+		vSlider,
+		vFooter,
   }
 }
 </script>
+<style lang="scss" scoped>
+.wrapper{
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+}
+</style>
